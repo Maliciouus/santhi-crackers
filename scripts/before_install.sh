0fc9551d2ced4a164e1a0da5db86d306b87dfe76
Maliciouus <aswinjothishsm@gmail.com>
2025-07-22 09:11:13 +0530

    crack

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,8 +2,12 @@
 set -e
 
 echo "======== [BeforeInstall] ========"
+
 sudo systemctl stop nginx || true
+
 rm -rf /var/www/html/build || true
-mkdir -p /var/www/html
+
 chown ec2-user:ec2-user /var/www/html
+
+mkdir -p /var/www/html
 chmod -R 755 /var/www/html
