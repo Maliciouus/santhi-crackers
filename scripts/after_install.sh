cb584f54135ab7cc834ce2b288ffb4ec6cc4b211
Maliciouus <aswinjothishsm@gmail.com>
2025-07-23 12:12:42 +0530

    done

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-unzip /var/www/html/client.zip -d /var/www/html/
-rm /var/www/html/client.zip
+unzip /var/www/html/client-bundle.zip -d /var/www/html/
+rm /var/www/html/client-bundle.zip
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,6 +3,10 @@ set -e
 
 echo "======== [BeforeInstall] ========"
 
+# Clean old deployment
+sudo rm -rf /var/www/html/*
+
+# Recreate folder and fix permissions
 sudo mkdir -p /var/www/html
 sudo chown ec2-user:ec2-user /var/www/html
 sudo chmod -R 755 /var/www/html
