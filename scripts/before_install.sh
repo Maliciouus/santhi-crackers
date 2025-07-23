61f8a2124cc62868818b1afb79d07e43567ffb2e
Maliciouus <aswinjothishsm@gmail.com>
2025-07-23 10:20:07 +0530

    done

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,10 +3,6 @@ set -e
 
 echo "======== [BeforeInstall] ========"
 
-sudo systemctl stop nginx || true
-
-sudo rm -rf /var/www/html/ || true
-
 sudo mkdir -p /var/www/html
 sudo chown ec2-user:ec2-user /var/www/html
 sudo chmod -R 755 /var/www/html
