3d8e22f9f8d911b478e3b03ae1c41fb146f9169d
Maliciouus <aswinjothishsm@gmail.com>
2025-07-23 10:25:56 +0530

    runit

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "======== [AfterInstall] Starting ========"
+echo "======== Installing NGINX ========"
 
-# Restart NGINX after deployment
-echo "Starting NGINX..."
+sudo yum update -y
+sudo amazon-linux-extras enable nginx1
+sudo yum install -y nginx
 sudo systemctl start nginx
-
-echo "======== [AfterInstall] Done ========"
+sudo systemctl enable nginx
